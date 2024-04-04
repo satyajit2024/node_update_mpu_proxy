42d9a084261465dcccde92b96dc04351e59cc034
satyajit2024 <satyajit.bariflo@outlook.com>
2024-04-04 17:18:51 +0530

    pr

--- proxi.py
+++ proxi.py
@@ -17,7 +17,6 @@ def count_pulse(channel):
     global pulses, last_times
 
     sensor_index = interrupt_pins.index(channel)
-    print(f"sensor_index.......{sensor_index}")
     current_time = time.time() * 1000  # Convert seconds to milliseconds
 
     if current_time - last_times[sensor_index] >= 1000:
@@ -41,12 +40,12 @@ for pin in interrupt_pins:
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(pin, GPIO.FALLING, callback=count_pulse)
 
-try:
-    print("Waiting for interrupts...")
-    while True:
-        time.sleep(1)
+# try:
+#     print("Waiting for interrupts...")
+#     while True:
+#         time.sleep(1)
 
-except KeyboardInterrupt:
-    # Handle keyboard interrupt (Ctrl+C)
-    GPIO.cleanup()
-    print("\nProgram terminated by user.")
+# except KeyboardInterrupt:
+#     # Handle keyboard interrupt (Ctrl+C)
+#     GPIO.cleanup()
+#     print("\nProgram terminated by user.")
