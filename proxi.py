6381fa4f1a18a02c9a5dcb18209fb32ab0e14621
satyajit2024 <satyajit.bariflo@outlook.com>
2024-04-04 17:17:44 +0530

    proxy

--- proxi.py
+++ proxi.py
@@ -17,6 +17,7 @@ def count_pulse(channel):
     global pulses, last_times
 
     sensor_index = interrupt_pins.index(channel)
+    print(f"sensor_index.......{sensor_index}")
     current_time = time.time() * 1000  # Convert seconds to milliseconds
 
     if current_time - last_times[sensor_index] >= 1000:
