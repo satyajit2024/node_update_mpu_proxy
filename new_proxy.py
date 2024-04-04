c4e364afc19c89f7f40d6acb75f8c35f72a6eb5a
satyajit2024 <satyajit.bariflo@outlook.com>
2024-04-04 17:56:26 +0530

    t

--- new_proxy.py
+++ new_proxy.py
@@ -17,9 +17,9 @@ def new_function():
 
 
 if __name__ == "__main__":
-    t1 = threading.Thread(target=new_function)
+    # t1 = threading.Thread(target=new_function)
     t2 = threading.Thread(target=hello)
-    t1.start()
+    # t1.start()
     t2.start()
-    t1.join()
+    # t1.join()
     t2.join()
