a4bdf9f0ab48d9951c9105685dd463bb52ee385f
satyajit2024 <satyajit.bariflo@outlook.com>
2024-04-04 17:23:01 +0530

    proxy

--- proxi.py
+++ proxi.py
@@ -40,12 +40,12 @@ for pin in interrupt_pins:
     GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
     GPIO.add_event_detect(pin, GPIO.FALLING, callback=count_pulse)
 
-# try:
-#     print("Waiting for interrupts...")
-#     while True:
-#         time.sleep(1)
+try:
+    print("Waiting for interrupts...")
+    # while True:
+    time.sleep(1)
 
-# except KeyboardInterrupt:
-#     # Handle keyboard interrupt (Ctrl+C)
-#     GPIO.cleanup()
-#     print("\nProgram terminated by user.")
+except KeyboardInterrupt:
+    # Handle keyboard interrupt (Ctrl+C)
+    GPIO.cleanup()
+    print("\nProgram terminated by user.")
